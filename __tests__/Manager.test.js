b07be7dc2184b52b2dbd4a18a0a574e49264c9bc
sacylkowski <sacylkowski@gmail.com>
2022-04-16 09:34:54 -0500

    adding code to manager

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,21 @@
+const Manager = require("../lib/Manager");
+const employeeValue = "Brian";
+const officeNum = 7;
+const idValue = 123;
+const emailValue = "email@test.com";
+
+test("get manager office number", () => {
+    const employee = new Manager(employeeValue, idValue, emailValue, officeNum);
+    expect(employee.officeNum).toEqual(officeNum);
+});
+
+test("can get office number when getOfficeNumber() is called", () => {
+    const employee = new Manager(employeeValue, idValue, emailValue, officeNum);
+    expect(employee.getId()).toBe(idValue);
+});
+
+test("can get manager role when getRole() is called", () => {
+    const manager = "manager"
+    const employee = new Manager(employeeValue, idValue, emailValue, officeNum);
+    expect(employee.getRole()).toBe(manager);
+});
